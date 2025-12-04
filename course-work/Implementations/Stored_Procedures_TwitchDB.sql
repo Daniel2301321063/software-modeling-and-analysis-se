db172bc07cb718bded9d24f3db315f4c97e1823e
Daniel2301321063 <stu2301321063@uni-plovdiv.bg>
2025-12-04 11:32:50 +0200

    Update Stored_Procedures_TwitchDB.sql

--- course-work/Implementations/Stored_Procedures_TwitchDB.sql
+++ course-work/Implementations/Stored_Procedures_TwitchDB.sql
@@ -8,14 +8,14 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Проверка за вече активен стрийм
     IF EXISTS (SELECT 1 FROM Streams WHERE ChannelId = @ChannelId AND IsLive = 1)
     BEGIN
-        RAISERROR(N'Каналът вече има активен стрийм.', 16, 1);
+        RAISERROR(N'ГЉГ Г­Г Г«ГєГІ ГўГҐГ·ГҐ ГЁГ¬Г  Г ГЄГІГЁГўГҐГ­ Г±ГІГ°ГЁГ©Г¬.', 16, 1);
         RETURN;
     END;
 
     INSERT INTO Streams(ChannelId, Title, StartedAt, IsLive, IsVOD)
     VALUES (@ChannelId, @Title, SYSUTCDATETIME(), 1, 0);
 END;
+
 GO
